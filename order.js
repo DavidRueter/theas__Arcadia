2af0586ed2f9f5d7fe34467cf8b93a364abe8d13
DavidRueter <drueter@assyst.com>
2020-11-09 23:30:40 -0800

    Commit from OpsStream spactGithubSWR

---
+++ order.js
@@ -0,0 +1,51 @@
+import vueTestJQ1 from {{ '/test/TestJQ1_App.vue'|theasResource }}
+
+  //use Axios instead of vue-resource for HTTP calls as per https://medium.com/the-vue-point/retiring-vue-resource-871a82880af4
+    Vue.prototype.$http = axios;
+
+    Vue.directive('swipeleft', {
+        bind: function (el, binding) {
+            if (typeof binding.value === 'function') {
+
+                const mc = new Hammer.Manager(el, {
+                    touchAction: 'auto',
+                    inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchMouseInput,
+                    recognizers: [
+                        [Hammer.Swipe, {
+                            direction: Hammer.DIRECTION_LEFT
+                         }]
+                     ]
+                 });
+                mc.on('swipeleft', binding.value);
+            }
+        }
+    });
+
+    Vue.directive('swiperight', {
+        bind: function (el, binding) {
+            if (typeof binding.value === 'function') {
+
+                 const mc = new Hammer.Manager(el, {
+                    touchAction: 'auto',
+                    inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchMouseInput,
+                    recognizers: [
+                        [Hammer.Swipe, {
+                            direction: Hammer.DIRECTION_RIGHT
+                         }]
+                     ]
+                 });
+            
+                mc.on('swiperight', binding.value);
+            }
+        }
+    });
+
+    if (!FormData.prototype.set) {
+        FormData.prototype.set = FormData.prototype.append;
+    }
+
+
+    new Vue({
+        delimiters: ["[[", "]]"],
+        render: h => h(vueTestJQ1),
+    }).$mount(`#thapp`);
