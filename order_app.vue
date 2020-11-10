c5ea1ffb35a440ce503146e372fc9a7f0f1bc047
DavidRueter <drueter@assyst.com>
2020-11-09 23:30:45 -0800

    Commit from OpsStream spactGithubSWR

---
+++ order_app.vue
@@ -0,0 +1,43 @@
+<template>
+    <b-container id="TestJQ1_Appvue" fluid="lg" style="padding: 0">
+
+        <b-row no-gutters fluid>
+            <b-col cols="12">
+                <h3>Bust that cache! For Sure!</h3>
+                <p>Hello World</p>
+            </b-col>
+        </b-row>
+
+        <b-row no-gutters>
+            <b-col cols="5">
+               <TestBSVTable></TestBSVTable>
+            </b-col>
+        </b-row>
+
+
+  <b-container>
+
+</template>
+ 
+<script>
+    import TestBSVTable from  {{ '/test/TestBSVTable2.vue'|theasResource }};
+
+ 
+    export default {
+        components: {
+            TestBSVTable
+        },
+        data: function () {
+            return {
+                placeholder: 1
+
+            }
+        },
+
+    }
+</script>
+
+<style>
+    h1   {color: blue;}
+    p    {color: red;}    
+</style>
