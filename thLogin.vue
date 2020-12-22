7d51eeffdd2c2d1d73143669344638a4135a8ca6
David Rueter <drueter@assyst.com>
2020-12-22 11:01:25 -0800

    WIP

--- thLogin.vue
+++ thLogin.vue
@@ -54,7 +54,7 @@
 </template>
 
 <script>
-    // Import any Vue components we want to use.
+    // We can mport any Vue components we want to use.
     // Note that these can be via theasResource filter (for
     // cache-busting), but they must specify a fully-qualified
     // path (i.e. must start with a / )
